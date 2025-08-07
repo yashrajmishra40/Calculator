f3528e74a123b99528776c14380658c67502f0a8
ANSHAM MAURYA <anshammaurya2291@gmail.com>
2025-08-07 14:53:58 +0530

    addition

---
+++ calculator.java
@@ -0,0 +1,13 @@
+package calculator;
+
+class Calculator{
+    public static int addititon(int a, int b){
+        return a + b;
+    }
+}
+
+public static class main{
+    public static void main(String[] args) {
+        int sum = Calculator.addition(1,4);
+    }
+}
