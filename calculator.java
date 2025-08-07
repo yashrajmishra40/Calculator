affd3b83f78e74cff36f3a18f4348ef6e127b540
ANSHAM MAURYA <143842290+ANSHAM1@users.noreply.github.com>
2025-08-07 15:35:09 +0530

    .

--- calculator.java
+++ calculator.java
@@ -8,6 +8,14 @@ public static class Calculator{
     public static int subtraction(int a, int b) {
         return a - b;
     }
+
+    public static int division(int a, int b) {
+        return a / b;
+    }
+
+    public static int multiplication(int a, int b) {
+        return a * b;
+    }
 }
 
 
