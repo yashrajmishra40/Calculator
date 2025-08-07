98c980ec71207899e4647a6435ad9b68e09f90bf
ANSHAM MAURYA <anshammaurya2291@gmail.com>
2025-08-07 16:09:20 +0530

    infinite loop

--- calculator.java
+++ calculator.java
@@ -8,6 +8,45 @@ public static class Calculator{
     public static int subtraction(int a, int b) {
         return a - b;
     }
+
+    public static int multiplication(int a, int b) {
+        return a * b;
+    }
+
+    public static int division(int a, int b) {
+        if(b == 0) return 0;
+        return a / b;
+    }
+
+    public static void main(String[] args) {
+        int result = 0;
+        int a = 4, b = 7;
+
+        while (true){
+            switch (condition) {
+                case "add"{
+                    result = addititon(a,b);
+                }
+
+                case "sub"{
+                    result = subtraction(a,b);
+                }
+
+                case "div"{
+                    result = division(a,b);
+                }
+
+                case "mul" {
+                    result = multiplication(a,b);
+                }
+
+                case "exit" {
+                    break;
+                }
+            }
+
+        }
+    }
 }
 
 
