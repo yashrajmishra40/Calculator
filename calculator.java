b246733a40139032b4e8a1311f762151b7a63461
yashrajmishra40 <mishrayashraj206@gmail.com>
2025-08-07 14:56:47 +0530

    Substraction done

---
+++ calculator.java
@@ -0,0 +1,13 @@
+package calculator;
+
+class Calculator {
+    public static int subtraction(int a, int b) {
+        return a - b;
+
+    }
+}
+ public static class main {
+    public static void main(String[] args) {
+        int substraction = Calculator.subtraction(10, 5);
+    }   
+ }
